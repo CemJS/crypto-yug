689ee9221bd4b3056cb9d72cbca1347895e3a984
quondem <86314992+quondem@users.noreply.github.com>
2024-03-13 10:33:07 +0300

    .

--- frontends/header/display/Main.tsx
+++ frontends/header/display/Main.tsx
@@ -7,7 +7,7 @@ export default function () {
       <div class="wrapper flex h-full items-center px-[0.625rem]">
         <nav
           class={[
-            "@700:flex @700:relative @700:top-0 @700:flex-row @700:gap-0 @700:pt-0 absolute bottom-0 left-0 top-[72px] hidden h-full w-full flex-col items-center justify-between gap-3 bg-neutral-900 pt-4 ring-0",
+            "absolute bottom-0 left-0 top-[72px] hidden h-[300px] w-full flex-col items-center justify-between gap-3 bg-neutral-900 py-4 ring-0 @700:relative @700:top-0 @700:flex @700:h-full @700:flex-row @700:gap-0 @700:pt-0",
             Static.active ? "!flex" : null,
           ]}
         >
@@ -15,7 +15,7 @@ export default function () {
             return (
               <a
                 href={item.link}
-                class="@700:block @1000:p-[0.625rem_1.5625rem] cursor-pointer whitespace-nowrap rounded-[0.875rem] p-[0.3125rem_0.9375rem] text-[0.75rem] [background:--greenGradient] [transition:all_1s_ease]"
+                class="cursor-pointer whitespace-nowrap rounded-[0.875rem] p-[0.3125rem_0.9375rem] text-[0.75rem] [background:--greenGradient] [transition:all_1s_ease] @700:block @1000:p-[0.625rem_1.5625rem]"
               >
                 {item.name}
               </a>
@@ -28,7 +28,7 @@ export default function () {
             e.target.classList.toggle("active");
             Static.active = !Static.active;
           }}
-          class="@700:hidden group ml-auto mr-3 w-[32px] cursor-pointer space-y-2 *:pointer-events-none *:[transition:all_0.4s_ease] [&.active_.first]:translate-y-[10px] [&.active_.first]:-rotate-45 [&.active_.second]:opacity-0 [&.active_.third]:translate-y-[-10px] [&.active_.third]:rotate-45"
+          class="group ml-auto mr-3 w-[32px] cursor-pointer space-y-2 *:pointer-events-none *:[transition:all_0.4s_ease] @700:hidden [&.active_.first]:translate-y-[10px] [&.active_.first]:-rotate-45 [&.active_.second]:opacity-0 [&.active_.third]:translate-y-[-10px] [&.active_.third]:rotate-45"
         >
           <span class="first block h-0.5 w-8 bg-white"></span>
           <span class="second block h-0.5 w-8 bg-white"></span>
