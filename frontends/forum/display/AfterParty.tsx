cd74974fdc0b14751464bae8d3ccc022561fde01
quondem <86314992+quondem@users.noreply.github.com>
2024-05-02 18:00:48 +0300

    .

--- frontends/forum/display/AfterParty.tsx
+++ frontends/forum/display/AfterParty.tsx
@@ -2,10 +2,23 @@ import { Cemjsx, Fn, Ref, Static } from "cemjs-all";
 
 export default function () {
   return (
-    <img
-      class="mx-auto w-full max-w-[1000px] py-8"
-      src="/contents/forum/afterParty.png"
-      alt="After party"
-    />
+    <div class="w-full text-center">
+      <h2 class="my-4 text-center text-[clamp(1.5rem,4vw,5.625rem)] font-bold">
+        After Party
+      </h2>
+      <h3 class="text-[clamp(1.3rem,3vw,2.8125rem)] font-bold">9 июня</h3>
+      <div class="flex w-full gap-2">
+        <img
+          class="my-8 w-[48%] rounded-[40px]"
+          src="/contents/forum/afterParty.png"
+          alt="After party"
+        />
+        <img
+          class="my-8 w-[48%] rounded-[40px]"
+          src="/contents/forum/vinnipuh.png"
+          alt="Jambey"
+        />
+      </div>
+    </div>
   );
 }
